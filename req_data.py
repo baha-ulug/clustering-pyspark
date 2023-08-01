b36acda5b58193a805319f25333baa108cdd94b1
baha-ulug <97911364+baha-ulug@users.noreply.github.com>
2023-08-01 11:52:19 +0300

    Update req_data.py

--- req_data.py
+++ req_data.py
@@ -1,16 +1,20 @@
 import requests
 from dotenv import load_dotenv
 import os
-load_dotenv()
+def download_data():   
+    load_dotenv()  # Loads environment variables from .env file
+    URL = os.environ.get("URL")
+    local_filename = "dataset/eda.csv"
+    response = requests.get(URL)
+    if response.status_code == 200:
+        with open(local_filename, "wb") as file:
+            file.write(response.content)
+        print(f"File downloaded successfully to the path {local_filename}.")
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
 
-URL = os.environ.get("URL")
-local_filename = "dataset/eda.csv"
+if __name__ == '__main__':
+    download_data()
 
-response = requests.get(URL)
 
-if response.status_code == 200:
-    with open(local_filename, "wb") as file:
-        file.write(response.content)
-    print("File downloaded successfully.")
-else:
-    print(f"Failed to download the file. Status code: {response.status_code}")
+
